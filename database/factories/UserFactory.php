ea653884c5e5ed69053fe886d6ff7d142ea2a324
Boris <bpzweber@gmail.com>
2019-11-21 11:36:30 +0100

    A Thread Should Be Assigned a Channel

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Thread;
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -31,8 +33,20 @@ $factory->define(App\Thread::class, function ($faker)  {
         'user_id' => function() {
             return factory('App\User')->create()->id;
         },
+        'channel_id' => function() {
+            return factory('App\Channel')->create()->id;
+        },
         'title' => $faker->sentence,
         'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(App\Channel::class, function ($faker)  {
+    $name = $faker->word;
+
+    return [
+        'name' => $name,
+        'slug' => $name
     ];
 });
 
